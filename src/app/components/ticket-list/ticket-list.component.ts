59fdd06e10df4f1ad8be6a429b8cc894e664c83c
mulwa <mulwatech@gmail.com>
2018-12-17 16:59:33 +0300

    adds ticket resources

--- src/app/components/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket-list/ticket-list.component.ts
@@ -21,7 +21,7 @@ export class TicketListComponent implements OnInit {
      let originalContents = document.body.innerHTML;
      document.body.innerHTML = printContents;    
     window.print();
-    document.body.innerHTML = originalContents;
+    // document.body.innerHTML = originalContents;
   }
 
 }
