fa89e1c8675ad493c0c28bba0d7391007f9bd69b
mulwa <mulwatech@gmail.com>
2018-12-17 17:26:30 +0300

    changes printer window

--- src/app/components/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket-list/ticket-list.component.ts
@@ -17,11 +17,28 @@ export class TicketListComponent implements OnInit {
   }
   printTicket(compName){
     console.log('invoking ticket printing')
-    let printContents = document.getElementById(compName).innerHTML;
-     let originalContents = document.body.innerHTML;
-     document.body.innerHTML = printContents;    
-    window.print();
+    // let printContents = document.getElementById(compName).innerHTML;
+    //  let originalContents = document.body.innerHTML;
+    //  document.body.innerHTML = printContents;    
+    // window.print();
     // document.body.innerHTML = originalContents;
+
+    let printContents, popupWin;
+    printContents = document.getElementById(compName).innerHTML;
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=auto,width=100%');
+    popupWin.document.open();
+    popupWin.document.write(`
+      <html>
+        <head>
+          <title>Print tab</title>
+          <style>
+          //........Customized style.......
+          </style>
+        </head>
+    <body onload="window.print();window.close()">${printContents}</body>
+      </html>`
+    );
+    popupWin.document.close();
   }
 
 }
